f8d58c1c0cd50bc8fd290916b9af4ff1007e748c
Hailey <hailey.m.w@gmail.com>
2014-02-19 22:03:29 -0800

    changed font, made nav bar colors change

--- scroller.js
+++ scroller.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	$('body').stellar();
+	$('.nav span').hover(function(){
+		$(this).css('background-color', '#E36F26');
+	},
+	function(){
+		$(this).css('background-color', '#E48025');
+	}
+	);
 });
